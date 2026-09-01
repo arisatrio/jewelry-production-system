c293518faf30f01cd31b27a86e15b27677e406cf
Ari Satrio <57087893+arisatrio@users.noreply.github.com>
2026-09-01 11:29:53 +0700

    build

---
+++ public/build/assets/Title-BzKENqhF.js
@@ -0,0 +1,1 @@
+import"./ResponsivePopover-D6A-r1MF.js";import{Tt as e}from"./app-B7WWJcCm.js";var t=e(`ui5-title`,[`level`,`size`,`wrappingType`],[],[],[]);t.displayName=`Title`;export{t};
--- public/build/assets/check-OYjd74zb.js
+++
@@ -1,1 +0,0 @@
-import{yt as e}from"./app-BM_Z9kr-.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
